d339d9999482c77c12e22a11f3b8ecba9bbfd775
Moeen Mahmud <write2fahim007@gmail.com>
2021-09-02 01:46:07 +0600

    forking around

--- js/app.js
+++ js/app.js
@@ -20,10 +20,10 @@ const displayResult = (docs) => {
     const authorsName = doc.author_name ? doc.author_name : "";
     const firstPublish = doc.first_publish_year ? doc.first_publish_year : "";
     const publisherName = doc.publisher ? doc.publisher : "";
-    const div = document.createElement("div");
+    const div = document.createElement("div", "space-y-2");
     div.innerHTML = `
-      <img src="https://covers.openlibrary.org/b/id/${coverImage}-M.jpg" class="block mx-auto">
-      <h1 class="text-xl font-bold">${bookName}</h1>
+      <img src="https://covers.openlibrary.org/b/id/${coverImage}-M.jpg" class="block mx-auto mb-4">
+      <h1 class="text-xl font-bold mb-2">${bookName}</h1>
       <h2 class="font-bold">${authorsName}</h2>
       <p class="text-gray-600">${firstPublish}</p>
       <p class="text-gray-600 font-bold">${publisherName}</p>
