5326f9b4f99a11be1056d95b77510e9cac0c816f
Moeen Mahmud <write2fahim007@gmail.com>
2021-09-02 11:19:43 +0600

    fixed issue

--- js/app.js
+++ js/app.js
@@ -9,33 +9,37 @@ const searchBook = async () => {
   const response = await fetch(url);
   const data = await response.json();
   displayResult(data.docs);
-  totalResults(data.numFound);
+  totalResults(data.docs.length);
 };
 
 const displayResult = (docs) => {
   docs.forEach((doc) => {
     doc.author_name?.forEach((author) => console.log(author));
-    const coverImage = doc.cover_i ? doc.cover_i : "10909258";
+    const coverImage = doc.cover_i ? doc.cover_i : "";
     const bookName = doc.title;
     const authorsName = doc.author_name ? doc.author_name : "";
     const firstPublish = doc.first_publish_year ? doc.first_publish_year : "";
-    const publisherName = doc.publisher ? doc.publisher : "";
-    const div = document.createElement("div", "space-y-2");
+    const [publisherName] = doc.publisher ? doc.publisher : "";
+    const div = document.createElement("div");
     div.innerHTML = `
-      <img src="https://covers.openlibrary.org/b/id/${coverImage}-M.jpg" class="block mx-auto mb-4">
+      <img src="https://covers.openlibrary.org/b/id/${coverImage}-M.jpg" class="block mx-auto mb-4 w-2/4 h-2/4">
       <h1 class="text-xl font-bold mb-2">${bookName}</h1>
-      <h2 class="font-bold">${authorsName}</h2>
-      <p class="text-gray-600">${firstPublish}</p>
-      <p class="text-gray-600 font-bold">${publisherName}</p>
-      
+      <h2 class="font-bold">By ${authorsName}</h2>
+      <p class="text-gray-600 mt-8"> First published in ${firstPublish}</p>
+      <p class="text-gray-600 font-bold">Publisher: ${publisherName}</p>
     `;
-    div.classList.add("thumbnails", "space-y-2");
+    div.classList.add("thumbnails");
     searchResult.appendChild(div);
-    // console.log(doc.title);
   });
 };
 
-const totalResults = (numFound) => {
+const totalResults = (totalFound) => {
   resultQuantity.classList.add("text-center");
-  resultQuantity.innerText = `Total ${numFound} results found`;
+  resultQuantity.innerHTML = `
+    <div class="block mx-auto">
+      <h1 class="text-xl font-bold text-indigo-500">
+        Total ${totalFound} results have found.
+      </h1>
+    </div>
+  `;
 };
