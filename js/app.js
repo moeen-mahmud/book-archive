2d83e9197ed6b6e744c54670c1f3a81d8a573487
Moeen Mahmud <write2fahim007@gmail.com>
2021-09-02 13:08:53 +0600

    added error handler

--- js/app.js
+++ js/app.js
@@ -1,45 +1,69 @@
-const searchField = document.getElementById("search-field");
 const searchResult = document.getElementById("search-result");
 const resultQuantity = document.getElementById("result-quantity");
-const errorContainer = document.getElementById("error-container");
 
 const searchBook = async () => {
+  const searchField = document.getElementById("search-field");
   const searchText = searchField.value;
-  const url = `https://openlibrary.org/search.json?q=${searchText}`;
-  const response = await fetch(url);
-  const data = await response.json();
-  displayResult(data.docs);
-  totalResults(data.docs.length);
+  searchField.value = "";
+  if (searchText === "") {
+    searchField.value = "Search field cannot be empty...";
+  } else {
+    const url = `https://openlibrary.org/search.json?q=${searchText}`;
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      displayResult(data.docs.slice(0, 30));
+      // console.log(data.docs.slice(0, 30));
+      totalResults(data.numFound);
+    } catch (err) {
+      totalResults(data.numFound);
+    }
+  }
 };
 
 const displayResult = (docs) => {
-  docs.forEach((doc) => {
-    doc.author_name?.forEach((author) => console.log(author));
-    const coverImage = doc.cover_i ? doc.cover_i : "";
-    const bookName = doc.title;
-    const authorsName = doc.author_name ? doc.author_name : "";
-    const firstPublish = doc.first_publish_year ? doc.first_publish_year : "";
-    const [publisherName] = doc.publisher ? doc.publisher : "";
-    const div = document.createElement("div");
-    div.innerHTML = `
+  searchResult.textContent = "";
+  if (docs.length > 0) {
+    docs.forEach((doc) => {
+      const coverImage = doc.cover_i ? doc.cover_i : "";
+      const bookName = doc.title;
+      const authorsName = doc.author_name ? doc.author_name : "";
+      const firstPublish = doc.first_publish_year ? doc.first_publish_year : "";
+      const publisherName = doc.publisher ? doc.publisher : "Not found";
+      const div = document.createElement("div");
+      div.innerHTML = `
       <img src="https://covers.openlibrary.org/b/id/${coverImage}-M.jpg" class="block mx-auto mb-4 w-2/4 h-2/4">
-      <h1 class="text-xl font-bold mb-2">${bookName}</h1>
-      <h2 class="font-bold">By ${authorsName}</h2>
+      <h1 class="mb-2 text-xl font-bold text-center">${bookName}</h1>
+      <h2 class="text-lg italic text-indigo-500">${authorsName}</h2>
       <p class="text-gray-600 mt-8"> First published in ${firstPublish}</p>
-      <p class="text-gray-600 font-bold">Publisher: ${publisherName}</p>
+      <p class="text-gray-600 font-bold">Publisher: ${publisherName.slice(
+        0,
+        1
+      )}</p>
     `;
-    div.classList.add("thumbnails");
-    searchResult.appendChild(div);
-  });
+      div.classList.add("thumbnails");
+      searchResult.appendChild(div);
+    });
+  }
 };
 
-const totalResults = (totalFound) => {
+const totalResults = (numFound) => {
   resultQuantity.classList.add("text-center");
-  resultQuantity.innerHTML = `
-    <div class="block mx-auto">
-      <h1 class="text-xl font-bold text-indigo-500">
-        Total ${totalFound} results have found.
+  if (numFound > 0) {
+    resultQuantity.innerHTML = `
+    <div class="block mx-auto mt-16">
+      <h1 class="text-xl font-bold text-indigo-500">Another 
+        ${numFound} results have been found.
       </h1>
     </div>
   `;
+  } else {
+    resultQuantity.innerHTML = `
+    <div class="block mx-auto mt-16">
+      <h1 class="text-5xl font-bold text-yellow-400">
+        Looks like there are no such books! 🤔
+      </h1>
+    </div>
+  `;
+  }
 };
